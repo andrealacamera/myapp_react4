348651ccd49ae555b44324632b51b39aed7c188a
andrealacamera <andrea.lacamera0@gmail.com>
2022-08-05 23:09:58 +0200

    setting build

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -6,8 +6,8 @@ const Page = () => {
   const {t} = useTranslation();
   return (
     <div>
-      <Trans t={t} i18nKey="message">
-        <p>text <Link to="https://teiga.it" target="_blank" >link</Link> and <BsPeople className='inline'></BsPeople> text.</p>
+      <Trans i18nKey="message">
+        <p className='text-violet-800 p-8 italic'>text <a className="hover:font-bold hover:underline" href="https://teiga.it" target="_blank" >link</a> and <BsPeople className='inline w-8 h-8'></BsPeople> text.</p>
       </Trans>
     </div>
   )
