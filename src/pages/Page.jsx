b984b4191375bb3c601a7072e5275ce56ef2ed2f
andrealacamera <andrea.lacamera0@gmail.com>
2022-08-06 11:40:39 +0200

    setting LanguageDetector and supportedLngs

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 import { BsPeople } from "react-icons/bs";
 
 const Page = () => {
-  const {t} = useTranslation();
+  const {t, i18n} = useTranslation();
+  const lang = i18n.language;
   return (
     <div>
       <Trans i18nKey="message">
         <p className='text-violet-800 p-8 italic'>text <a className="hover:font-bold hover:underline" href="https://teiga.it" target="_blank" >link</a> and <BsPeople className='inline w-8 h-8'></BsPeople> text.</p>
       </Trans>
+      <p className='p-8'>L = {lang}</p>
     </div>
   )
 }
